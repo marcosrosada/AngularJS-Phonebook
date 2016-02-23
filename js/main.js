b44a659696c0fea6a352356693baab0087da8372
Marcos Rosada <ochild@gmail.com>
2016-02-23 02:43:51 -0300

    Add Contact feature

--- js/main.js
+++ js/main.js
@@ -9,4 +9,9 @@ app.controller("contactsCtrl", function($scope){
 		{ name: "Talita Vespa", number: +5521979276633 },
 		{ name: "Joniorrrrrrr", number: +552799998888 }
 	];
+
+	$scope.addContact = function(contact){
+		$scope.listContacts.push( angular.copy(contact));
+		delete $scope.contact;
+	};
 });
