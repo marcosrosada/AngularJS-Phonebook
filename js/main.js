5811c0aae63b369b8b4b2b9695a67cd2c4d048ad
Marcos Rosada <ochild@gmail.com>
2016-02-23 00:53:53 -0300

    Add file .gitignore

---
+++ js/main.js
@@ -0,0 +1,12 @@
+app = angular.module("Application", []);
+
+app.controller("contactsCtrl", function($scope){
+
+	$scope.title = "List";
+
+	$scope.list = [
+		{ name: "Marcos Rosada", number: +5521980147515 },
+		{ name: "Talita Vespa", number: +5521979276633 },
+		{ name: "Joniorrrrrrr", number: +552799998888 }
+	];
+});
