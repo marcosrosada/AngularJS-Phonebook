785f568d64c59a0ff406c5c8c476acb8aea68c81
Marcos Rosada <ochild@gmail.com>
2016-02-23 01:56:32 -0300

    Adding style and features to table

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ app.controller("contactsCtrl", function($scope){
 
 	$scope.title = "Phonebook Application";
 
-	$scope.list = [
+	$scope.listContacts = [
 		{ name: "Marcos Rosada", number: +5521980147515 },
 		{ name: "Talita Vespa", number: +5521979276633 },
 		{ name: "Joniorrrrrrr", number: +552799998888 }
